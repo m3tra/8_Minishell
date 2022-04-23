e3b57a2a9693c1abd79e062f74b30ffefb8411b5
fporto <fporto@student.42lisboa.com>
2022-04-24 00:54:18 +0100

    vai toma

---
+++ inc/minishell.h
@@ -0,0 +1,11 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+// # include "libft.h"
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fporto <fporto@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/23 21:29:25 by fporto            #+#    #+#             */
+/*   Updated: 2022/04/24 00:23:29 by fporto           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	main(int argc, char **argv, char **env)
+{
+	char	*input;
+	char	**params;
+
+	for (int i = 0; env[i]; i++)
+		printf("%s\n", env[i]);
+	while (1)
+	{
+		// show_prompt();
+		// read_command(input, params);
+	}
+
+
+	return (0);
+}
